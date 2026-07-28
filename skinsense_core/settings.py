f5d0457869b29d749051d11b5bcce3bb0fa1ebbc
Poonam Banga <poonam2247rintern@gmail.com>
2026-07-29 04:48:22 +0530

    Update settings for deployment, ignore uploaded media files

--- skinsense_core/settings.py
+++ skinsense_core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5-hxi*tr*whl*o&iu0jq(ii5e^=-qe+azp76blv*ui*1o2@y)$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
