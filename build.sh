466e405a1def77e870760602d072ac2cbb3b8c74
Poonam Banga <poonam2247rintern@gmail.com>
2026-07-29 05:33:27 +0530

    Create default user on deployment

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); User.objects.filter(username='Poonam').exists() or User.objects.create_user('Poonam', password='Poonam@Skin')"
